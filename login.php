193622ebc080e83aa95fdeaafa89c9a33a836415
belajarbahasapro <siswanto_23510020@stimata.ac.id>
2024-05-25 10:57:32 +0700

    kasir

--- login.php
+++ login.php
@@ -49,7 +49,7 @@ require 'function.php';
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Achmad Nur Rozikin 2024</div>
+                            <div class="text-muted">Copyright &copy; Siswanto 2024</div>
                         </div>
                     </div>
                 </footer>
--- masuk.php
+++ masuk.php
@@ -1,7 +1,3 @@
-<?php
-require 'function.php';
-$barang = mysqli_query($koneksi, "SELECT * FROM produk");
-?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -61,55 +57,61 @@ $barang = mysqli_query($koneksi, "SELECT * FROM produk");
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4">Stock Barang</h1>
+                        <h1 class="mt-4">Dashboard</h1>
                         <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Jumlah Barang :</div>
+                                    <div class="card-body">Primary Card</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
-                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
-                                        Tambah Barang
-                                    </button>
-                                    <div class="container mt-3">
-                                    </div>
+                                </div>
                             </div>
-                            
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-table me-1"></i>
-                                Data Stock Barang
+                                DataTable Example
                             </div>
                             <div class="card-body">
                                 <table id="datatablesSimple">
                                     <thead>
                                         <tr>
-                                            <th>No</th>
-                                            <th>Nama Produk</th>
-                                            <th>Deskripsi</th>
-                                            <th>Harga</th>
-                                            <th>Stock</th>
+                                            <th>Name</th>
+                                            <th>Position</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Salary</th>
                                         </tr>
                                     </thead>
-                                    <tbody>
-                                        <?php $i = 1; ?>
-                                        <?php foreach ($barang as $brg) : ?>
+                                    <tfoot>
                                         <tr>
-                                            <td><?= $i; ?></td>
-                                            <td><?= $brg['nama_produk']; ?></td>
-                                            <td><?= $brg['deskripsi']; ?></td>
-                                            <td><?= $brg['harga']; ?></td>
-                                            <td><?= $brg['stock']; ?></td>
+                                            <th>Name</th>
+                                            <th>Position</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Salary</th>
                                         </tr>
-                                        <?php $i++; ?>
-                                        <?php endforeach; ?>
+                                    </tfoot>
+                                    <tbody>
+                                        <tr>
+                                            <td>Tiger Nixon</td>
+                                            <td>System Architect</td>
+                                            <td>Edinburgh</td>
+                                            <td>61</td>
+                                            <td>2011/04/25</td>
+                                            <td>$320,800</td>
+                                        </tr>
                                     </tbody>
                                 </table>
                             </div>
                         </div>
                     </div>
-                </main>
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
@@ -117,7 +119,6 @@ $barang = mysqli_query($koneksi, "SELECT * FROM produk");
                         </div>
                     </div>
                 </footer>
-            </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
@@ -126,32 +127,3 @@ $barang = mysqli_query($koneksi, "SELECT * FROM produk");
         <script src="assets/demo/chart-bar-demo.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
         <script src="js/datatables-simple-demo.js"></script>
-    </body>
-    <!-- Modal -->
-<div class="modal" id="myModal">
-  <div class="modal-dialog">
-    <div class="modal-content">
-
-      <!-- Modal Header -->
-      <div class="modal-header">
-        <h4 class="modal-title">Tambah Data Barang</h4>
-        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-      </div>
-      <form method="POST">
-      <!-- Modal body -->
-      <div class="modal-body">
-        <input type="text" name="nama_produk" class="form-control mt-3" placeholder="nama produk">
-        <input type="text" name="deskripsi" class="form-control mt-3" placeholder="deskripsi produk">
-        <input type="num" name="harga" class="form-control mt-3" placeholder="harga">
-        <input type="num" name="stock" class="form-control mt-3" placeholder="stock">
-      </div>
-      <!-- Modal footer -->
-      <div class="modal-footer">
-        <button type="submit" class="btn btn-success" name="tambahproduk">Simpan</button>
-        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Tutup</button>
-      </div>
-      </form>
-    </div>
-  </div>
-</div>
-</html>
